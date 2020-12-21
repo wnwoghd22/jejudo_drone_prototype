64660d6e1dca05c7169370e444599f5f345726df
wnwoghd22 <53481297+wnwoghd22@users.noreply.github.com>
2020-12-21 23:43:09 +0900

    initial
    initialized server develop environment

---
+++ server/functions/index.js
@@ -0,0 +1,5 @@
+import { user } from "../functions/js/user";
+
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp("https://jejudo-drone-prototype-default-rtdb.firebaseio.com/");
---
+++ server/functions/js/user.js
@@ -0,0 +1,5 @@
+export function user(name, id, pw) {
+    this.name = name;
+    this.id = id;
+    this.pw = pw;
+};
