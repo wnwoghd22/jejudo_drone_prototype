0750aa0511ae17d75c8cc2652ae3bcad25f8c0d2
wnwoghd22 <53481297+wnwoghd22@users.noreply.github.com>
2021-01-18 17:34:22 +0900

    fixing
    CORS issue

--- server/functions/index.js
+++ server/functions/index.js
@@ -5,14 +5,69 @@
 
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
-admin.initializeApp("https://jejudo-drone-prototype-default-rtdb.firebaseio.com/");
+admin.initializeApp({databaseURL:`https://jejudo-drone-prototype-default-rtdb.firebaseio.com/`});
 
 const express = require('express');
 const app = express();
 
-const cors = require('cors') ({origin: true});
+const cors = require('cors')({origin: true});
 app.use(cors);
 
-//export { provider, app };
+const anonymous = {
+    id : "anon",
+    name : "Anonymous"
+}
+const CheckUser = (req, res, next) => {
+    req.user = anonymous;
+    if(req.query.auth_token != undefined) {
+        let idToken = req.query.auth_token;
+        admin.auth().verifyIdToken(idToken).then(decodedToken => {
+            let authUser = {
+                id: decodedToken.user_id,
+                name: decodedToken.name
+            };
+            req.user = authUser;
+            next();
+        }).catch(error => {
+            next();
+        });
+    }
+    else {
+        next();
+    }
+}
+app.use(CheckUser);
+
+app.get('/announcements', (req, res) => {
+    let listRef = admin.database().ref('announcements');
+    listRef.once('value', function(snapshot) {
+        let items = new Array();
+        snapshot.forEach(function(childSnapshot) {
+            let contents = childSnapshot.val();
+            contents.id = childSnapshot.key;
+            items.push(contents);
+        })
+        res.header('Content-Type', 'application/json; charset = utf-8');
+        res.send({announcements: items});
+    })
+});
+app.post('/announcements', (req, res) => {
+    let contents = {
+        date: new Date().toJSON(),
+        body: req.body.body,
+        user: req.body.user
+    };
+    let listRef = admin.database().ref(`announcements`);
+    listRef.push(contents);
+    res.header('Content-Type', 'application/json; charset = utf-8');
+    res.status(201).send({result: "post complete"});
+});
+function initAnnounceList() {
+    let listRef = admin.database().ref('announcements');
+    listRef = {
+        
+    };
+    //temporary
+}
 
 exports.v1 = functions.https.onRequest(app);
